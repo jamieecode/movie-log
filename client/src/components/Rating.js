030a0aa2a41af57df98dfbb8d54443a027c1773a
Jaewan <jamieecode@gmail.com>
2021-09-18 00:54:00 +0900

    fix: star rating component

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -19,7 +19,7 @@ const Rating = ({ value }) => {
     const [whole, part] = parseFloat(value).toString().split(".");
     for (let i = 0; i < whole; i++) stars.push(100);
     if (part) stars.push(50);
-    for (let i = whole; i < (part ? 4 : 5); i++) stars.push(0);
+    for (let i = whole; i < (part ? 9 : 10); i++) stars.push(0);
 
     return stars;
   };
